f8d1822d93b389d31a964ece9454d6802ac5198c
WigiTech <accounts@wigitech.com>
2018-10-11 10:30:10 +0300

    first commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyDEJCWY1TQcVU-Q5GznqKm6RXb2xPs7Bm8",
+    authDomain: "mayastore-1106c.firebaseapp.com",
+    databaseURL: "https://mayastore-1106c.firebaseio.com",
+    projectId: "mayastore-1106c",
+    storageBucket: "",
+    messagingSenderId: "750640822143"
+};
